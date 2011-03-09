3da41ecf6cabb620f7ec005a069903cfa443be15
socks <socks@dev.null>
2011-03-08 16:15:47 -0800

    Commented out problematic code. Probably part of an incomplete future feature.

--- LoLAdvanced/Core.cpp
+++ LoLAdvanced/Core.cpp
@@ -18,7 +18,7 @@ CCore::Initialize( void )
 
 	Game_Loop = m_cMemory.Patch<Game_Loop_t>( CMemory::Call, (DWORD) Game_Loop, (DWORD) New_Game_Loop );
 	
-	DebugCircleManager::GetSingleton( )->AddDebugCircle( Unit::GetPlayerToon( ), 10.0f, 0xAAAAAAAA );
+	// DebugCircleManager::GetSingleton( )->AddDebugCircle( Unit::GetPlayerToon( ), 10.0f, 0xAAAAAAAA );
 
 	return true;
 }
